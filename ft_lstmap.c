707a6f58325c53639e916dd09ce391a78f8d18df
Alfonse <alfonse.brown.6767@gmail.com>
2021-11-02 13:28:18 +0100

    finish lstmap and fix triteri

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,30 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tbrandt <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/27 15:50:04 by tbrandt           #+#    #+#             */
-/*   Updated: 2021/10/29 14:48:15 by tbrandt          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	
-	if (!lst)
-		return (NULL);
-	new_list = malloc(sizeof(t_list));
+	t_list	*newlist;
+	t_list	*new_link;
+
+	newlist = NULL;
 	while (lst)
 	{
-		new_list->content = f(lst->content);
-		del(lst->content);
+		new_link = ft_lstnew(f(lst->content));
+		if (!new_link)
+		{
+			del(newlist->content);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlist, new_link);
 		lst = lst->next;
-		ft_lstdelone(lst, del);
 	}
-	return (new_list);
+	return (newlist);
 }
